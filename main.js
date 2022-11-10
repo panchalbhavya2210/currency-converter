601ff0f67447b86433101dba4d3fdd7186a171a0
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-11-10 21:02:02 +0530

    commit deskcurr

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 let btn = document.getElementById("submit");
 
-btn.addEventListener('click', convert)
+btn.addEventListener("click", convert);
+
+let currency = document.getElementById("currency");
+
+currency.addEventListener("keyup", () => {
+  let currency = document.getElementById("currency").value;
+  let warnToggMinus = document.getElementById("warnToggMinus");
+
+  if (currency < "0" && currency != "") {
+    btn.disabled = true;
+    warnToggMinus.classList.add("warnEmToggle");
+  } else if (currency == "") {
+    btn.disabled = false;
+    warnToggMinus.classList.remove("warnEmToggle");
+  } else {
+    btn.disabled = false;
+    warnToggMinus.classList.remove("warnEmToggle");
+  }
+});
 
 function convert() {
   let currency = document.getElementById("currency").value;
+
   let toXon = document.getElementById("toConvert");
   let toCon = toXon.value;
   let forXon = document.getElementById("forConvert");
@@ -15,56 +34,54 @@ function convert() {
   if (toCon == forCon) {
     fieldTogg.classList.add("warnEmToggle");
     setTimeout(() => {
-      fieldTogg.classList.remove('warnEmToggle');
-    }, 3000)
-    isEmt.innerHTML = "Please select different currency..."
-  }
-  else if (toCon == "empty" || forCon == "empty") {
+      fieldTogg.classList.remove("warnEmToggle");
+    }, 3000);
+    isEmt.innerHTML = "Please select different currency...";
+  } else if (toCon == "empty" || forCon == "empty") {
     fieldTogg.classList.add("warnEmToggle");
     setTimeout(() => {
-      fieldTogg.classList.remove('warnEmToggle');
-    }, 3000)
+      fieldTogg.classList.remove("warnEmToggle");
+    }, 3000);
     isEmt.innerHTML = "Please select both currencies...";
-  }
-  else if (currency == "") {
-    warnTogg.classList.add('warnEmToggle');
+  } else if (currency == "") {
+    warnTogg.classList.add("warnEmToggle");
     setTimeout(() => {
-      warnTogg.classList.remove('warnEmToggle');
-    }, 3000)
-  }
-  else {
+      warnTogg.classList.remove("warnEmToggle");
+    }, 3000);
+  } else {
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com',
-        'X-RapidAPI-Key': '809a0bf07dmsh81685045fb7b3dcp1605c9jsn13035c2f0611'
-      }
+        "X-RapidAPI-Host":
+          "currency-conversion-and-exchange-rates.p.rapidapi.com",
+        "X-RapidAPI-Key": "809a0bf07dmsh81685045fb7b3dcp1605c9jsn13035c2f0611",
+      },
     };
 
-    fetch(`https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=${toCon}&to=${forCon}&amount=${currency}`, options)
-      .then(response => {
+    fetch(
+      `https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=${toCon}&to=${forCon}&amount=${currency}`,
+      options
+    )
+      .then((response) => {
         if (response.status == 200) {
           let responseClass = document.getElementById("xResult");
-          
-          xResult.style.opacity = '1';
+
+          xResult.style.opacity = "1";
         }
-        return response.json()
+        return response.json();
       })
-      .then(data => {
+      .then((data) => {
         console.log(data);
         let fResult = document.getElementById("finalResult");
         let mRound = data.result;
         fResult.innerHTML = mRound.toFixed(2);
-      })
+      });
 
     let valOne = document.getElementById("valOne");
     let valTwo = document.getElementById("valTwo");
     let x = document.getElementById("x");
     let y = document.getElementById("y");
     let inpRes = document.getElementById("inputResult");
-    //  let fResult = document.getElementById("finalResult");
-
-
 
     x.innerHTML = toCon;
     y.innerHTML = forCon;
